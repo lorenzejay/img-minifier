ee1a9d7532b635eaeb595597fb8c6a42285e3a36
Lorenze Jay Hernandez <63378463+lorenzejay@users.noreply.github.com>
2022-08-10 06:57:27 -0700

    Lj drag automation setup #1)
    * setting where uploaded files will end up
    
    * code cleanup

---
+++ README.md
@@ -0,0 +1,5 @@
+minifying photos - png & jpg.
+
+- [ ] set the folder you want to minify to - though this is a manual typing process since there is no gui yet.
+- [ ] Automation when you drag to folder
+    - [ ] set output path and then drag to upload folder to get the job done
--- app.py
+++ app.py
@@ -1,30 +1,29 @@
-# todos
-# [] check if file exists compressed then ask
-# [] clear files in compressed folder?
-
-
 from pickletools import optimize
 from PIL import Image
 import os
  
 cwd = os.getcwd()
 
-def compress_images(directory=False, quality=30):
+def compress_images(directory=False, quality=30, customExportFolder=False):
+  compressedFilesLocation = cwd+"/compressed"
   # if there is a directory change to it 
   if directory:
     os.chdir(directory)
   # listing out the files inside path 
   files = os.listdir()
+  if customExportFolder:
+    if not os.path.exists(customExportFolder):
+      os.mkdir(customExportFolder)
+    compressedFilesLocation = customExportFolder
+  elif not os.path.exists(compressedFilesLocation):
+    os.mkdir(compressedFilesLocation)
 
   images = [file for file in files if file.endswith(('jpg', 'png', "JPG"))]
   for image in images:
     splitImageUrl = os.path.splitext(image)
     img = Image.open(image)
-    #open in a new directory called compressed
-    if not os.path.isdir(f"{cwd}/compressed"):
-      os.mkdir(f"{cwd}/compressed")
-    # saving with a new name
-    img.save(f"{cwd}/compressed/"+splitImageUrl[0]+"_compressed"+splitImageUrl[1], optimize=True, quality=quality)
+    img.save(f"{compressedFilesLocation}/"+splitImageUrl[0]+"_compressed"+splitImageUrl[1], optimize=True, quality=quality)
 
-compress_images("uploads", 50)
+
+# compress_images("uploads", 50)
 
