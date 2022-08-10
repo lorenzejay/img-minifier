b96d046f083f9812e90c672fb34b2cabce693701
Lorenze Jay <lorenzejaytech@gmail.com>
2022-08-10 06:54:47 -0700

    setting where uploaded files will end up

--- app.py
+++ app.py
@@ -9,22 +9,36 @@ import os
  
 cwd = os.getcwd()
 
-def compress_images(directory=False, quality=30):
+def compress_images(directory=False, quality=30, customExportFolder=False):
+  compressedFilesLocation = cwd+"/compressed"
   # if there is a directory change to it 
   if directory:
     os.chdir(directory)
   # listing out the files inside path 
   files = os.listdir()
+  if customExportFolder:
+    if not os.path.exists(customExportFolder):
+      os.mkdir(customExportFolder)
+    compressedFilesLocation = customExportFolder
+  elif not os.path.exists(compressedFilesLocation):
+    os.mkdir(compressedFilesLocation)
 
   images = [file for file in files if file.endswith(('jpg', 'png', "JPG"))]
   for image in images:
     splitImageUrl = os.path.splitext(image)
     img = Image.open(image)
-    #open in a new directory called compressed
-    if not os.path.isdir(f"{cwd}/compressed"):
-      os.mkdir(f"{cwd}/compressed")
-    # saving with a new name
-    img.save(f"{cwd}/compressed/"+splitImageUrl[0]+"_compressed"+splitImageUrl[1], optimize=True, quality=quality)
+    # print('splitImageUrl:',splitImageUrl) 
+    # print(compressedFilesLocation)
+    img.save(f"{compressedFilesLocation}/"+splitImageUrl[0]+"_compressed"+splitImageUrl[1], optimize=True, quality=quality)
+
+# compress_images("uploads", 50)
 
 compress_images("uploads", 50)
+# def fireFromCustomFolder(folderName=False):
+#   print(folderName)
+#   if not os.path.isdir(folderName):
+#     os.mkdir(folderName)
+#   else:
 
+
+# fireFromCustomFolder(f"{cwd}/compressed-test")
